e644559f946bf3c030930e7ae9cdebeacb7b822a
kluradowee <kigoftedash@gmail.com>
2024-09-28 22:15:21 +0330

    Create file.js

---
+++ file.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById('menu');
+
+        function openclose(val) {
+            if (val == 'open') {
+                menu.style.display = 'block';
+            }else if (val == 'close') {
+                menu.style.display = 'none';
+            }
+        }
